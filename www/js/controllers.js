92ed15ff4fe725a45fe25b3e8680dcf4109256a3
LordJavito <javisemi@gmail.com>
2014-11-16 15:54:28 +0100

    Primera subida

---
+++ www/js/controllers.js
@@ -0,0 +1,10 @@
+angular.module('app.controllers', [])
+
+.controller('HomeCtrl', function($scope) {
+})
+
+.controller('ListaCtrl', function($scope) {
+})
+
+.controller('CestaCtrl', function($scope, Friends) {
+})
